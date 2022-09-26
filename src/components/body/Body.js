7e14339b9e3d195b36f358288bd0811eaf859cbd
Dighol khan <digholkhan@gmail.com>
2022-09-26 20:03:36 +0600

    Project finished

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,17 +3,17 @@ import Menu from "./Menu";
 import Home from "./Home";
 import Contact from "./Contact";
 import About from "./About"
-import { Route,Routes,Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 const Body = () => {
-    return(
+    return (
         <div>
-           <Routes>
-                <Route path="/home" exeact element={<Home/>}/>
-                <Route path="/menu" exeact element={<Menu/>}/>
-                <Route path="/Contact" exeact element={<Contact/>}/>
-                <Route path="/about" exeact element={<About/>}/>
-                <Route path='/'  element={<Navigate replace to="/home" />} />
-           </Routes>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/Contact" element={<Contact />} />
+                <Route path="/about" element={<About />} />
+                <Route path='/' element={<Navigate replace to="/home" />} />
+            </Routes>
         </div>
     );
 }
